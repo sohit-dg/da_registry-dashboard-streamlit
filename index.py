117d23a53922b054a54466752e58704dcf8d759d
Sohit <sohit@digitalgreen.org>
2024-05-19 23:37:38 +0530

    (impl) : da by region

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import streamlit as st
 import psycopg2
 import os
 import pandas as pd
+import plotly.express as px
 
 load_dotenv()
 st.set_page_config(
@@ -32,8 +33,32 @@ def fetch_data(query, params=None):
     connection.close()
     return data
 
+def master_query():
+    query = """
+            SELECT 
+                COUNT(da.id) as total_no_of_das,
+                COUNT(da.id) / COUNT(DISTINCT k.id) as average_da_per_kebele,
+                SUM(CASE WHEN g.name = 'Male' THEN 1 ELSE 0 END) as count_male_da,
+                SUM(CASE WHEN g.name = 'Female' THEN 1 ELSE 0 END) as count_female_da,
+                COUNT(DISTINCT s.id) as total_specialization,
+                COUNT(DISTINCT r.id) as total_regions,
+                COUNT(DISTINCT z.id) as total_zones,
+                COUNT(DISTINCT w.id) as total_woredas,
+                COUNT(DISTINCT k.id) as total_kebeles
+            FROM registry_developmentagent da
+            JOIN registry_kebele k ON da.kebele_id = k.id
+            JOIN registry_woreda w ON k.woreda_id = w.id
+            JOIN registry_zone z ON w.zone_id = z.id
+            JOIN registry_region r ON z.region_id = r.id
+            JOIN registry_gender g ON da.gender_id = g.id
+            JOIN registry_educationlevel el ON da.education_level_id = el.id
+            JOIN registry_specialization s ON da.specialization_id = s.id
+            WHERE 1=1
+    """
+    return query
+
 # Function to build the master query with filters
-def master_query(gender_filter, education_level_filter, specialization_filter, region_filter, zone_filter, woreda_filter, kebele_filter):
+def master_query_with_filter(gender_filter, education_level_filter, specialization_filter, region_filter, zone_filter, woreda_filter, kebele_filter):
     query = """
             SELECT 
                 COUNT(da.id) as total_no_of_das,
@@ -87,6 +112,71 @@ def get_das_by_region():
     """
     return query
 
+def get_das_by_education_level(gender_filter, education_level_filter, specialization_filter, region_filter, zone_filter, woreda_filter, kebele_filter):
+    query = """
+    SELECT 
+    COUNT(da.id) as total_no_of_das,
+    el."name" as education_name
+    FROM registry_developmentagent da
+    JOIN registry_kebele k ON da.kebele_id = k.id
+    JOIN registry_woreda w ON k.woreda_id = w.id
+    JOIN registry_zone z ON w.zone_id = z.id
+    JOIN registry_region r ON z.region_id = r.id
+    JOIN registry_gender g ON da.gender_id = g.id
+    JOIN registry_educationlevel el ON da.education_level_id = el.id
+    JOIN registry_specialization s ON da.specialization_id = s.id
+    WHERE 1=1
+    """
+    if region_filter != 'none':
+        query += f" AND r.name = '{region_filter}'"
+    if gender_filter != 'none':
+        query += f" AND g.name = '{gender_filter}'"
+    if education_level_filter != 'none':
+        query += f" AND el.name = '{education_level_filter}'"
+    if specialization_filter != 'none':
+        query += f" AND s.name = '{specialization_filter}'"
+    if zone_filter != 'none':
+        query += f" AND z.name = '{zone_filter}'"
+    if woreda_filter != 'none':
+        query += f" AND w.name = '{woreda_filter}'"
+    if kebele_filter != 'none':
+        query += f" AND k.name = '{kebele_filter}'"
+    query += " GROUP BY el.name"
+    return query
+
+def get_das_by_specialisations(gender_filter, education_level_filter, specialization_filter, region_filter, zone_filter, woreda_filter, kebele_filter):
+    query = """
+    SELECT 
+    COUNT(da.id) as total_no_of_das,
+    s."name" as spl_name
+    FROM registry_developmentagent da
+    JOIN registry_kebele k ON da.kebele_id = k.id
+    JOIN registry_woreda w ON k.woreda_id = w.id
+    JOIN registry_zone z ON w.zone_id = z.id
+    JOIN registry_region r ON z.region_id = r.id
+    JOIN registry_gender g ON da.gender_id = g.id
+    JOIN registry_educationlevel el ON da.education_level_id = el.id
+    JOIN registry_specialization s ON da.specialization_id = s.id
+    WHERE 1=1
+    """
+    
+    if region_filter != 'none':
+        query += f" AND r.name = '{region_filter}'"
+    if gender_filter != 'none':
+        query += f" AND g.name = '{gender_filter}'"
+    if education_level_filter != 'none':
+        query += f" AND el.name = '{education_level_filter}'"
+    if specialization_filter != 'none':
+        query += f" AND s.name = '{specialization_filter}'"
+    if zone_filter != 'none':
+        query += f" AND z.name = '{zone_filter}'"
+    if woreda_filter != 'none':
+        query += f" AND w.name = '{woreda_filter}'"
+    if kebele_filter != 'none':
+        query += f" AND k.name = '{kebele_filter}'"
+    query += " GROUP BY s.name"
+    return query
+
 # Functions to fetch filter options
 def fetch_specializations():
     query = "SELECT DISTINCT name FROM registry_specialization"
@@ -128,6 +218,90 @@ def main():
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     """, unsafe_allow_html=True)
     
+    master_data = fetch_data(master_query())
+    result = master_data[0]
+    # Assign the values to descriptive variable names
+    total_no_of_das = result[0]
+    average_da_per_kebele = result[1]
+    count_male_da = result[2]
+    count_female_da = result[3]
+    total_specialization = result[4]
+    total_regions = result[5]
+    total_zones = result[6]
+    total_woredas = result[7]
+    total_kebeles = result[8]
+
+    data = {
+        "Gender": ["Male", "Female"],
+        "Count": [count_male_da, count_female_da]
+    }
+    df = pd.DataFrame(data)
+
+    # Calculate total and percentages
+    df['Percentage'] = (df['Count'] / df['Count'].sum()) * 100
+
+    # Display results
+    percentage_male = df.loc[df['Gender'] == 'Male', 'Percentage'].values[0]
+    percentage_female = df.loc[df['Gender'] == 'Female', 'Percentage'].values[0]
+    das_by_region = fetch_data(get_das_by_region())
+    
+    das_by_region_df = pd.DataFrame(das_by_region, columns=['count', 'region'])
+    total_count = das_by_region_df['count'].sum()
+    das_by_region_df['percentage'] = (das_by_region_df['count'] / total_count * 100).round(2)
+    percentages_by_region = {row['region']: f"{row['percentage']}%" for _, row in das_by_region_df.iterrows()}
+    
+    region_percentages_html = ""
+    for region, percentage in percentages_by_region.items():
+        region_percentages_html += f'<div class="sub-list-title"><span class="bullet_green">&#8226;</span> {region}: {percentage}</div>\n'
+    
+    st.subheader("Total Stats")
+    st.markdown(f"""
+        <div class="grid_cont">
+                <div class="basic_card">
+                    <div class="title">Total no. of DAs</div>
+                    <div class="sub-title"><span class="bullet_green">&#8226;</span> {total_no_of_das}</div>
+                </div>
+                <div class="basic_card">
+                    <div class="title">Average DA per kebele</div>
+                    <div class="sub-title"><span class="bullet_green">&#8226;</span> {average_da_per_kebele}</div>
+                </div>
+                <div class="basic_card">
+                    <div class="title">DA by gender</div>
+                    <div class="gender-info"><span class="bullet_green">&#8226;</span> Male: {percentage_male:.2f}%</div>
+                    <div class="gender-info"><span class="bullet_orange">&#8226;</span> Female: {percentage_female:.2f}%</div>
+                </div>
+                <div class="basic_card">
+                    <div class="title">Total specialisations</div>
+                    <div class="sub-title"><span class="bullet_green">&#8226;</span> {total_specialization}</div>
+                </div>
+                <div class="basic_card">
+                    <div class="title">DAs by region</div>
+                    <div class="flex_container">
+                        <div class="region_percentage_container">{region_percentages_html}</div>
+                    </div>
+                </div>
+                <div class="basic_card">
+                    <div class="title">Location stats</div>
+                    <div class="basic_card_row">
+                        <div class="basic_card_row_item"><span class="bullet_yellow">&#8226;</span> Region: {total_regions}</div>
+                        <div class="basic_card_row_item"><span class="bullet_green">&#8226;</span> Zone: {total_zones}</div>
+                    </div>
+                    <div class="basic_card_row">
+                        <div class="basic_card_row_item"><span class="bullet_blue">&#8226;</span> Woreda: {total_woredas}</div>
+                        <div class="basic_card_row_item"><span class="bullet_orange">&#8226;</span> Kebele: {total_kebeles}</div>
+                    </div>
+                </div>
+        </div>
+    """, unsafe_allow_html=True)
+    # fig = px.pie(das_by_region_df, values='count', names='region', title='DAs by Region')
+    # col1, col2 = st.columns([1, 1])
+    # with col1:
+    #     with st.container(border=True):
+    #         st.plotly_chart(fig, use_container_width=True)
+    # with col2:
+    #     with st.container(border=True):
+    #         st.plotly_chart(fig, use_container_width=True)
+    
     st.subheader("Filter By")
     col21, col22, col23, col24, col25, col26, col27 = st.columns([1, 1, 1, 1, 1, 1, 1])
     
@@ -162,96 +336,57 @@ def main():
     education_level_filter = col26.selectbox("Education Level", education_levels)
     specialization_filter = col27.selectbox("Specialization", specializations)
     
-    master_data = fetch_data(master_query(gender_filter, education_level_filter, specialization_filter, region_filter, zone_filter, woreda_filter, kebele_filter))
-    result = master_data[0]
-    # Assign the values to descriptive variable names
-    total_no_of_das = result[0]
-    average_da_per_kebele = result[1]
-    count_male_da = result[2]
-    count_female_da = result[3]
-    total_specialization = result[4]
-    total_regions = result[5]
-    total_zones = result[6]
-    total_woredas = result[7]
-    total_kebeles = result[8]
-
-    data = {
+    master_data_with_filter = fetch_data(master_query_with_filter(gender_filter, education_level_filter, specialization_filter, region_filter, zone_filter, woreda_filter, kebele_filter))
+    filtered_result = master_data_with_filter[0]
+    filtered_total_no_of_das = filtered_result[0]
+    filtered_average_da_per_kebele = filtered_result[1]
+    filtered_count_male_da = filtered_result[2]
+    filtered_count_female_da = filtered_result[3]
+    filtered_total_regions = filtered_result[5]
+    filtered_total_zones = filtered_result[6]
+    filtered_total_woredas = filtered_result[7]
+    filtered_total_kebeles = filtered_result[8]
+    
+    filtered_data_obj = {
         "Gender": ["Male", "Female"],
-        "Count": [count_male_da, count_female_da]
+        "Count": [filtered_count_male_da, filtered_count_female_da]
     }
-    df = pd.DataFrame(data)
+    filtered_df = pd.DataFrame(filtered_data_obj)
 
     # Calculate total and percentages
-    df['Percentage'] = (df['Count'] / df['Count'].sum()) * 100
+    filtered_df['Percentage'] = (filtered_df['Count'] / filtered_df['Count'].sum()) * 100
 
     # Display results
-    percentage_male = df.loc[df['Gender'] == 'Male', 'Percentage'].values[0]
-    percentage_female = df.loc[df['Gender'] == 'Female', 'Percentage'].values[0]
-    das_by_region = fetch_data(get_das_by_region())
-    
-    das_by_region_df = pd.DataFrame(das_by_region, columns=['count', 'region'])
-    total_count = das_by_region_df['count'].sum()
-    das_by_region_df['percentage'] = (das_by_region_df['count'] / total_count * 100).round(2)
-    percentages_by_region = {row['region']: f"{row['percentage']}%" for _, row in das_by_region_df.iterrows()}
-    
-    region_percentages_html = ""
-    for region, percentage in percentages_by_region.items():
-        region_percentages_html += f'<div class="sub-list-title"> {region}: {percentage}</div>\n'
-    
-    st.subheader("Total Stats")
+    filtered_percentage_male = filtered_df.loc[filtered_df['Gender'] == 'Male', 'Percentage'].values[0]
+    filtered_percentage_female = filtered_df.loc[filtered_df['Gender'] == 'Female', 'Percentage'].values[0]
+    st.subheader("DA by region")
     st.markdown(f"""
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
+        <div class="grid_cont">
                 <div class="basic_card">
                     <div class="title">Total no. of DAs</div>
-                    <div class="sub-title"><span class="bullet_green">&#8226;</span> {total_no_of_das}</div>
+                    <div class="sub-title"><span class="bullet_green">&#8226;</span> {filtered_total_no_of_das}</div>
                 </div>
-            </div>
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
                 <div class="basic_card">
                     <div class="title">Average DA per kebele</div>
-                    <div class="sub-title"><span class="bullet_green">&#8226;</span> {average_da_per_kebele}</div>
+                    <div class="sub-title"><span class="bullet_green">&#8226;</span> {filtered_average_da_per_kebele}</div>
                 </div>
-            </div>
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
                 <div class="basic_card">
                     <div class="title">DA by gender</div>
-                    <div class="gender-info"><span class="bullet_green">&#8226;</span> Male: {percentage_male:.2f}%</div>
-                    <div class="gender-info"><span class="bullet_orange">&#8226;</span> Female: {percentage_female:.2f}%</div>
+                    <div class="gender-info"><span class="bullet_green">&#8226;</span> Male: {filtered_percentage_male:.2f}%</div>
+                    <div class="gender-info"><span class="bullet_orange">&#8226;</span> Female: {filtered_percentage_female:.2f}%</div>
                 </div>
-            </div>
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
-                <div class="basic_card">
-                    <div class="title">Total specialisations</div>
-                    <div class="sub-title"><span class="bullet_green">&#8226;</span> {total_specialization}</div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
-                <div class="basic_card">
-                    <div class="title">DAs by region</div>
-                    <div class="flex_container">
-                        <div>chart</div>
-                        <div class="sub-list-title">{region_percentages_html}</div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12">
                 <div class="basic_card">
                     <div class="title">Location stats</div>
                     <div class="basic_card_row">
-                        <div class="basic_card_row_item"><span class="bullet_yellow">&#8226;</span> Region: {total_regions}</div>
-                        <div class="basic_card_row_item"><span class="bullet_green">&#8226;</span> Zone: {total_zones}</div>
+                        <div class="basic_card_row_item"><span class="bullet_yellow">&#8226;</span> Region: {filtered_total_regions}</div>
+                        <div class="basic_card_row_item"><span class="bullet_green">&#8226;</span> Zone: {filtered_total_zones}</div>
                     </div>
                     <div class="basic_card_row">
-                        <div class="basic_card_row_item"><span class="bullet_blue">&#8226;</span> Woreda: {total_woredas}</div>
-                        <div class="basic_card_row_item"><span class="bullet_orange">&#8226;</span> Kebele: {total_kebeles}</div>
+                        <div class="basic_card_row_item"><span class="bullet_blue">&#8226;</span> Woreda: {filtered_total_woredas}</div>
+                        <div class="basic_card_row_item"><span class="bullet_orange">&#8226;</span> Kebele: {filtered_total_kebeles}</div>
                     </div>
                 </div>
-            </div>
         </div>
-    </div>
     """, unsafe_allow_html=True)
-
 if __name__ == "__main__":
-    main()
+    main()     
